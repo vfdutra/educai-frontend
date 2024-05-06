d75b7a880f8e5118864cf3dd34565a9a7317806d
Henrique Lengruber <henriquerl22@outlook.com>
2024-05-06 17:22:08 -0300

    style: header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import * as React from 'react';
-import MainPage from './pages/MainPage/mainPage.component';
+import * as React from "react";
+import MainPage from "./pages/MainPage/mainPage.component";
+import { BorderBottom } from "@mui/icons-material";
 
 function App() {
+  const headerStyle = {
+    backgroundColor: "#F8F6E3",
+    color: "#333",
+    padding: "10px 52px",
+    textAlign: "left",
+    fontSize: "24px",
+    fontWeight: "bold",
+    BorderBottom: "1px solid #6AD4DD",
+  };
+
+  const globalStyle = {
+    body: {
+      margin: 0,
+      padding: 0,
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">      
-      <MainPage/>
+      <style>
+        {`
+          body {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      </style>
+      <header className="App-header" style={headerStyle}>
+        EducAI
       </header>
+      <MainPage />
     </div>
   );
 }
